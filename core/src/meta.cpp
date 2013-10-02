d2ba7bba03017ec2746962687efe2bc7a52ee4e9
Boris Temkin <boris@temk.org>
2013-10-02 17:36:30 +0300

    ~

---
+++ core/src/meta.cpp
@@ -0,0 +1,37 @@
+#include <ds/meta.h>
+#include <stdexcept>
+
+
+using namespace ds;
+using namespace std;
+
+meta::~meta() {
+}
+
+
+bool
+meta::contains(const string &key) const {
+    return map_.count(key) > 0;
+}
+
+const string &
+meta::get(const string &key) const {
+    map_t::const_iterator i = map_.find(key);
+    if (i == map_.end()) {
+        throw runtime_error("no such key: " + key);
+    }
+
+    return i -> second;
+}
+
+void
+meta::set(const string &key, const string &val) {
+    map_[key] = val;
+}
+
+void
+meta::keys(vector<string> &out) const {
+    for (map_t::const_iterator i = map_.begin(); i != map_.end(); ++ i) {
+        out.push_back(i ->second);
+    }
+}
