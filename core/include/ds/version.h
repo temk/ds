e1312837de24fef08fb86d00f9dfd221ea5e4426
Boris Temkin <boris@temk.org>
2015-03-01 17:03:43 +0200

    version support

---
+++ core/include/ds/version.h
@@ -0,0 +1,15 @@
+#ifndef __DS_VERSION_H__
+#define __DS_VERSION_H__
+
+namespace ds {
+
+	/**
+	 * current version
+	**/
+	static const int MAJOR_VERSION = 1;
+	static const int MINOR_VERSION = 1;
+	static const int BUILD_VERSION = 1;
+
+}
+
+#endif // __DS_VERSION_H__
