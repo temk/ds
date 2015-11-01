f2519fdf04c98c0824f796d523c3abb708a2b476
Boris Temkin <boris@katros.net>
2015-11-01 11:39:50 +0200

    max column name length in matlab enlarged from 128 to 1024

--- matlab/mex/ds_add.cpp
+++ matlab/mex/ds_add.cpp
@@ -13,7 +13,7 @@ using namespace ds;
 using namespace std;
 
 #define MAX_TYPE_LEN 12
-#define MAX_NAME_LEN 128
+#define MAX_NAME_LEN 1024
 #define MAX_ENDIAN_LEN 12
 
 
@@ -21,25 +21,25 @@ using namespace std;
 /**
  * ds_add(handle, int type, const char *name, const char *endian, int64 idx, int32 siz, uint64 width);
 **/
-void 
+void
 mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
-    if (nrhs != 7 || !mxIsUint64(prhs[0]) || !mxIsChar(prhs[1]) || !mxIsChar(prhs[2]) || !mxIsChar(prhs[3]) || 
+    if (nrhs != 7 || !mxIsUint64(prhs[0]) || !mxIsChar(prhs[1]) || !mxIsChar(prhs[2]) || !mxIsChar(prhs[3]) ||
                      !mxIsInt64(prhs[4])  || !mxIsChar(prhs[5]) || !mxIsUint64(prhs[6]) ) {
 		mexErrMsgTxt("expected 7 input argument: handle, string type, string name, string endian, int64 idx, int32 siz, width");
 	}
-	
+
 	char str_type[MAX_TYPE_LEN];
 	char str_name[MAX_NAME_LEN];
 	char str_endian[MAX_ENDIAN_LEN];
-	
+
 	mxGetString(prhs[1], &str_type[0], MAX_TYPE_LEN);
 	mxGetString(prhs[2], &str_name[0], MAX_NAME_LEN);
 	mxGetString(prhs[3], &str_endian[0], MAX_ENDIAN_LEN);
-	
+
     uint64_t width = *(uint64_t *)mxGetData(prhs[6]);
 	int64_t ix = *(int64_t *)mxGetData(prhs[4]);
-	
+
 	ds_handle stor;
 	try {
 		stor.h = *(uint64_t *)mxGetData(prhs[0]);
